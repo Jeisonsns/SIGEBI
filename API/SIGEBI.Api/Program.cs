ca38ec83f541abf168a35d34de45a778d50de6bd
CGMA TECHNOLOGY <CGMA TECHNOLOGY@DESKTOP-5HI8DG3>
2026-04-11 06:00:43 -0400

    Correcciones API

--- API/SIGEBI.Api/Program.cs
+++ API/SIGEBI.Api/Program.cs
@@ -21,7 +21,7 @@ var mongoClient = new MongoClient(mongoSettings);
 var mongoDatabase = mongoClient.GetDatabase(databaseName);
 builder.Services.AddSingleton<IMongoDatabase>(mongoDatabase);
 
-// Dependencias SIGEBIs
+// Dependencias SIGEBI
 builder.Services.AddSIGEBIDependencies();
 
 // Controladores con soporte de enums como string
